34fa5f275acc92558ba85b4f1d6f2c87a994007f
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-01 15:06:05 +0530

    final commit

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,32 @@
 //write your code here
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
 
 export default useFetch
